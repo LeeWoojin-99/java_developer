5a3460a76fd688ff829d867b48419d9154591960
LeeWoojin-99 <lwj1316@naver.com>
2022-10-05 18:18:11 +0900

    1005

--- Visual Studio Code/src/LWJ.java
+++ Visual Studio Code/src/LWJ.java
@@ -7,9 +7,10 @@ public class LWJ {
         int arr[][] = {{1,2},{2,3},{3,4},{2,4}};
 
         Arrays.sort(arr, (a, b)->{
-            if(a[0] == b[0]){
-                return a[1] - b[1];
+            if(a[0] == b[0]){ // x좌표의 값이 같다면
+                return a[1] - b[1]; // y좌표를 오름차순 정렬
             }else return a[0] - b[0];
+            // x좌표의 값이 다르다면 x좌표를 오름차순 정렬
         });
 
         for(int i[]: arr){
