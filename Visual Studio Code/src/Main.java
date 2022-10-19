44eb5a94e3f291956fc6689002d770c148f7ab55
LeeWoojin-99 <lwj1316@naver.com>
2022-10-19 18:09:03 +0900

    1019

--- Visual Studio Code/src/Main.java
+++ Visual Studio Code/src/Main.java
@@ -1,38 +1,15 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.StringTokenizer;
-/* 
-5
-2 4 -10 4 -9
+import java.util.Scanner;
 
-6
-1000 999 1000 999 1000 999
- */
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        System.out.println(factorial(sc.nextInt()));
+        sc.close();
 
-        int n = Integer.parseInt(br.readLine()); // n 입력
-
-        // 좌표 입력
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        // 입력된 좌표를 리스트에 담기
-        ArrayList<Integer> list = new ArrayList<>();
-        while(st.hasMoreTokens()) list.add(Integer.parseInt(st.nextToken()));
-
-        Object[] sortedArr = list.stream().distinct().sorted().toArray();
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<sortedArr.length; i++) map.put((int)sortedArr[i], i);
-
-        StringBuilder sb = new StringBuilder();
-        for(int i: list) sb.append(map.get(i)+" ");
-
-        // 출력부
-        System.out.print(sb);
+    }
+    static int factorial(int num){
+        if(num > 1) return num*factorial(num-1);
+        return 1;
     }
 }
