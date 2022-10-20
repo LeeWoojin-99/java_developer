0ac9a681bc2ac518e781472e30dad83b6b2e78d6
LeeWoojin-99 <lwj1316@naver.com>
2022-10-20 17:53:04 +0900

    1020

--- Visual Studio Code/src/Main.java
+++ Visual Studio Code/src/Main.java
@@ -1,15 +1,25 @@
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println(factorial(sc.nextInt()));
-        sc.close();
+        int n = 10;
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(0);
+        System.out.println(Fibonacci(list, n));
 
     }
-    static int factorial(int num){
-        if(num > 1) return num*factorial(num-1);
-        return 1;
+    static int Fibonacci(ArrayList<Integer> list, int count){
+        // System.out.println(list);
+        int sum = list.get(list.size()-1)+list.get(list.size()-2);
+        if(count < 1) return list.get(list.size()-1);
+        if(sum == 0) {
+            list.add(1);
+            return Fibonacci(list, --count);
+        }
+        list.add(sum);
+        return Fibonacci(list, --count);
     }
+    
 }
